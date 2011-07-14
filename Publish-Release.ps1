b2888f34f28165a9f50ff5c167a08fab9322432c
Tatham Oddie <tatham@oddie.com.au>
2011-07-14 11:04:51 +1000

    Added Publish-Release.ps1 script for building and uploading NuGet packages.

---
+++ Publish-Release.ps1
@@ -0,0 +1,28 @@
+﻿param (
+	[Parameter(Mandatory=$true)]
+	[ValidatePattern("\d\.\d\.\d\.\d")]
+	[string]
+	$ReleaseVersionNumber
+)
+
+$ErrorActionPreference = "Stop"
+
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+
+$SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+
+# Build the NuGet package
+$ProjectPath = Join-Path -Path $SolutionRoot -ChildPath "SnowMaker\SnowMaker.csproj"
+& nuget pack $ProjectPath -Symbols -Prop Configuration=Release -OutputDirectory $SolutionRoot
+if (-not $?)
+{
+	throw "The NuGet process returned an error code."
+}
+
+# Upload the NuGet package
+$NuPkgPath = Join-Path -Path $SolutionRoot -ChildPath "SnowMaker.$ReleaseVersionNumber.nupkg"
+& nuget push $NuPkgPath
+if (-not $?)
+{
+	throw "The NuGet process returned an error code."
+}
