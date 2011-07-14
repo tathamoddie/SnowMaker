a7d1e7a66c751bb00793a20b7ccbe74e3b573388
Tatham Oddie <tatham@oddie.com.au>
2011-07-14 17:05:25 +1000

    Exposed SnowMaker interals to test project.

--- SnowMaker/Properties/AssemblyInfo.cs
+++ SnowMaker/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SnowMaker")]
@@ -15,3 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.0.1.1")]
 [assembly: AssemblyFileVersion("0.0.1.1")]
+
+[assembly: InternalsVisibleTo("SnowMaker.UnitTests")]
